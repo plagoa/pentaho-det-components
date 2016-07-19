4929f5c7ab39868a539c54b0fb7b6588b44cb876
Francisco Guilherme <fguilherme@pentaho.com>
2016-07-19 14:59:32 +0100

    Adds lodash dependency

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ require.config({
 
   paths: {
     'jquery'				    : '../bower_components/jquery/dist/jquery',
+    'lodash'				    : '../bower_components/lodash/lodash',
     'angular'				    : '../bower_components/angular/angular',
     'text'              : '../bower_components/text/text',
     'css'               : '../bower_components/require-css/css'
