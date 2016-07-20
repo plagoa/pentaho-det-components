1f66397dca8e8f3fba8e422d1f67597b629e8cae
Francisco Guilherme <fguilherme@pentaho.com>
2016-07-20 11:08:13 +0100

    Adds fields grouping by categories

--- src/app/services/mock.service.js
+++ src/app/services/mock.service.js
@@ -1,19 +1,23 @@
 define(
-  [],
-  function() {
+  [
+    'lodash'
+  ],
+  function(_) {
     'use strict';
 
     function mockService() {
 
       var fields = [
-        {name: 'Field 1', state: false},
-        {name: 'Field 2', state: false},
-        {name: 'Field 3', state: false},
-        {name: 'Field 4', state: false},
-        {name: 'Field 5', state: false},
-        {name: 'Field 11', state: false},
-        {name: 'Field 12', state: false},
-        {name: 'Field 13', state: false}
+        {name: 'Field 1', state: false, category: 'Category 1'},
+        {name: 'Field 2', state: false, category: 'Category 1'},
+        {name: 'Field 3', state: false, category: 'Category 1'},
+        {name: 'Field 4', state: false, category: 'Category 2'},
+        {name: 'Field 5', state: false, category: 'Category 2'},
+        {name: 'Field 6', state: false, category: 'Category 2'},
+        {name: 'Field 7', state: false, category: 'Category 2'},
+        {name: 'Field 8', state: false, category: 'Category 3'},
+        {name: 'Field 9', state: false, category: 'Category 3'},
+        {name: 'Field 10', state: false, category: 'Category 3'}
       ];
 
 
@@ -33,6 +37,10 @@ define(
         return fields;
       }
 
+      function getCategories() {
+        return _.groupBy(fields, 'category');;
+      }
+
       function fieldSelected(selectedField) {
         fields.forEach(function(field){
           if(field.name == selectedField.name) field.state = !selectedField.state;
@@ -42,6 +50,7 @@ define(
       return {
         getFields: getFields,
         getDropZones: getDropZones,
+        getCategories: getCategories,
         fieldSelected: fieldSelected
       }
     }
@@ -49,4 +58,3 @@ define(
     return mockService;
   }
 );
-
