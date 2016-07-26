467238355dfe7a1c148eb3f92107f1f84243cf22
candentira <viktoryia_burchyk@epam.com>
2016-07-26 14:19:48 +0300

    [BACKLOG-9318] - fixing formating for mock service

--- src/app/tab/utils/utils-mock.service.js
+++ src/app/tab/utils/utils-mock.service.js
@@ -23,197 +23,344 @@ define(
 
     function MockService() {
 
-      var fields = [
-       {id: "1", name: 'Field 1', state: false, category: 'Category 1'},
-       {id: "2", name: 'Field 2', state: false, category: 'Category 1'},
-       {id: "3", name: 'Field 3', state: false, category: 'Category 1'},
-       {id: "4", name: 'Field 4', state: false, category: 'Category 2'},
-       {id: "5", name: 'Field 5', state: false, category: 'Category 2'},
-       {id: "6", name: 'Field 6', state: false, category: 'Category 2'},
-       {id: "7", name: 'Field 7', state: false, category: 'Category 2'},
-       {id: "8", name: 'Field 8', state: false, category: 'Category 3'},
-       {id: "9", name: 'Field 9', state: false, category: 'Category 3'},
-       {id: "10", name: 'Field 10', state: false, category: 'Category 3'},
-       {id: "11", name: 'Field 11', state: false, category: 'Category 3'},
-       {id: "12", name: 'Field 12', state: false, category: 'Category 3'},
-       {id: "13", name: 'Field 13', state: false, category: 'Category 3'},
-       {id: "14", name: 'Field 14', state: false, category: 'Category 3'}
-     ];
+      var fields = [{
+        id: "1",
+        name: 'Field 1',
+        state: false,
+        category: 'Category 1'
+      }, {
+        id: "2",
+        name: 'Field 2',
+        state: false,
+        category: 'Category 1'
+      }, {
+        id: "3",
+        name: 'Field 3',
+        state: false,
+        category: 'Category 1'
+      }, {
+        id: "4",
+        name: 'Field 4',
+        state: false,
+        category: 'Category 2'
+      }, {
+        id: "5",
+        name: 'Field 5',
+        state: false,
+        category: 'Category 2'
+      }, {
+        id: "6",
+        name: 'Field 6',
+        state: false,
+        category: 'Category 2'
+      }, {
+        id: "7",
+        name: 'Field 7',
+        state: false,
+        category: 'Category 2'
+      }, {
+        id: "8",
+        name: 'Field 8',
+        state: false,
+        category: 'Category 3'
+      }, {
+        id: "9",
+        name: 'Field 9',
+        state: false,
+        category: 'Category 3'
+      }, {
+        id: "10",
+        name: 'Field 10',
+        state: false,
+        category: 'Category 3'
+      }, {
+        id: "11",
+        name: 'Field 11',
+        state: false,
+        category: 'Category 3'
+      }, {
+        id: "12",
+        name: 'Field 12',
+        state: false,
+        category: 'Category 3'
+      }, {
+        id: "13",
+        name: 'Field 13',
+        state: false,
+        category: 'Category 3'
+      }, {
+        id: "14",
+        name: 'Field 14',
+        state: false,
+        category: 'Category 3'
+      }];
 
 
-     var drop_zones = [
-       {name: 'dropZone1', id: 'dp1', label: 'Drop Zone 1', maxFields: 1, emptyLabel: 'Drop Field Here', active: false, disabled: false, required: true,
-           currentFields: [
-           ],
-           acceptFields: [
-               {id: "1", name: 'Field 1', state: false, category: 'Category 1'},
-               {id: "2", name: 'Field 2', state: false, category: 'Category 1'},
-               {id: "3", name: 'Field 3', state: false, category: 'Category 1'}
-           ]},
-       {name: 'dropZone2', id: 'dp2', label: 'Drop Zone 2', maxFields: 5, emptyLabel: 'Drop Numeric Field Here', active: false, disabled: false, required: false,
-           currentFields:[
+      var drop_zones = [{
+        name: 'dropZone1',
+        id: 'dp1',
+        label: 'Drop Zone 1',
+        maxFields: 1,
+        emptyLabel: 'Drop Field Here',
+        active: false,
+        disabled: false,
+        required: true,
+        currentFields: [],
+        acceptFields: [{
+          id: "1",
+          name: 'Field 1',
+          state: false,
+          category: 'Category 1'
+        }, {
+          id: "2",
+          name: 'Field 2',
+          state: false,
+          category: 'Category 1'
+        }, {
+          id: "3",
+          name: 'Field 3',
+          state: false,
+          category: 'Category 1'
+        }]
+      }, {
+        name: 'dropZone2',
+        id: 'dp2',
+        label: 'Drop Zone 2',
+        maxFields: 5,
+        emptyLabel: 'Drop Numeric Field Here',
+        active: false,
+        disabled: false,
+        required: false,
+        currentFields: [
 
-           ],
-           acceptFields: [
-               {id: "1", name: 'Field 1', state: false, category: 'Category 1'},
-               {id: "2", name: 'Field 2', state: false, category: 'Category 1'},
-               {id: "3", name: 'Field 3', state: false, category: 'Category 1'}
-           ]},
-       {name: 'dropZone3', id: 'dp3', label: 'Drop Zone 3', maxFields: 7, emptyLabel: 'Drop Any Field Here', active: false, disabled: false, required: true,
-           currentFields:[
+        ],
+        acceptFields: [{
+          id: "1",
+          name: 'Field 1',
+          state: false,
+          category: 'Category 1'
+        }, {
+          id: "2",
+          name: 'Field 2',
+          state: false,
+          category: 'Category 1'
+        }, {
+          id: "3",
+          name: 'Field 3',
+          state: false,
+          category: 'Category 1'
+        }]
+      }, {
+        name: 'dropZone3',
+        id: 'dp3',
+        label: 'Drop Zone 3',
+        maxFields: 7,
+        emptyLabel: 'Drop Any Field Here',
+        active: false,
+        disabled: false,
+        required: true,
+        currentFields: [
 
-           ],
-           acceptFields: [
-               {id: "4", name: 'Field 4', state: false, category: 'Category 2'},
-               {id: "5", name: 'Field 5', state: false, category: 'Category 2'},
-               {id: "6", name: 'Field 6', state: false, category: 'Category 2'},
-               {id: "7", name: 'Field 7', state: false, category: 'Category 2'},
-               {id: "8", name: 'Field 8', state: false, category: 'Category 3'},
-               {id: "9", name: 'Field 9', state: false, category: 'Category 3'},
-               {id: "10", name: 'Field 10', state: false, category: 'Category 3'}
-           ]},
-       {name: 'dropZone4', id: 'dp4', label: 'Drop Zone 4', maxFields: 3, emptyLabel: 'Drop Any Field Here', active: false, disabled: false, required: false,
-           currentFields:[
+        ],
+        acceptFields: [{
+          id: "4",
+          name: 'Field 4',
+          state: false,
+          category: 'Category 2'
+        }, {
+          id: "5",
+          name: 'Field 5',
+          state: false,
+          category: 'Category 2'
+        }, {
+          id: "6",
+          name: 'Field 6',
+          state: false,
+          category: 'Category 2'
+        }, {
+          id: "7",
+          name: 'Field 7',
+          state: false,
+          category: 'Category 2'
+        }, {
+          id: "8",
+          name: 'Field 8',
+          state: false,
+          category: 'Category 3'
+        }, {
+          id: "9",
+          name: 'Field 9',
+          state: false,
+          category: 'Category 3'
+        }, {
+          id: "10",
+          name: 'Field 10',
+          state: false,
+          category: 'Category 3'
+        }]
+      }, {
+        name: 'dropZone4',
+        id: 'dp4',
+        label: 'Drop Zone 4',
+        maxFields: 3,
+        emptyLabel: 'Drop Any Field Here',
+        active: false,
+        disabled: false,
+        required: false,
+        currentFields: [
 
-           ],
-           acceptFields: [
-               {id: "4", name: 'Field 4', state: false, category: 'Category 2'},
-               {id: "5", name: 'Field 5', state: false, category: 'Category 2'},
-               {id: "6", name: 'Field 6', state: false, category: 'Category 2'},
-               {id: "7", name: 'Field 7', state: false, category: 'Category 2'},
-               {id: "8", name: 'Field 8', state: false, category: 'Category 3'},
-               {id: "9", name: 'Field 9', state: false, category: 'Category 3'},
-               {id: "10", name: 'Field 10', state: false, category: 'Category 3'}
-           ]}
-     ];
+        ],
+        acceptFields: [{
+          id: "4",
+          name: 'Field 4',
+          state: false,
+          category: 'Category 2'
+        }, {
+          id: "5",
+          name: 'Field 5',
+          state: false,
+          category: 'Category 2'
+        }, {
+          id: "6",
+          name: 'Field 6',
+          state: false,
+          category: 'Category 2'
+        }, {
+          id: "7",
+          name: 'Field 7',
+          state: false,
+          category: 'Category 2'
+        }, {
+          id: "8",
+          name: 'Field 8',
+          state: false,
+          category: 'Category 3'
+        }, {
+          id: "9",
+          name: 'Field 9',
+          state: false,
+          category: 'Category 3'
+        }, {
+          id: "10",
+          name: 'Field 10',
+          state: false,
+          category: 'Category 3'
+        }]
+      }];
 
-     function getDropZones() {
-       return drop_zones;
-     }
+      function getDropZones() {
+        return drop_zones;
+      }
 
 
-     function getFields() {
-       return fields;
-     }
+      function getFields() {
+        return fields;
+      }
 
-     function getCategories() {
-       return _.groupBy(fields, 'category');;
-     }
+      function getCategories() {
+        return _.groupBy(fields, 'category');;
+      }
 
-     function fieldSelected(selectedField) {
-       fields.forEach(function(field){
-         if(field.name == selectedField.name) {
-             field.state = !selectedField.state;
-         }
-       });
-     }
+      function fieldSelected(selectedField) {
+        fields.forEach(function(field) {
+          if (field.name == selectedField.name) {
+            field.state = !selectedField.state;
+          }
+        });
+      }
 
 
-     function dropZoneDropActive(selectedField, status) {
-         for(var zone in drop_zones)
-         {
-             var found = false;
-             var zone_items = drop_zones[zone].acceptFields;
-             for(var i=0; i < zone_items.length; i++)
-             {
-                 var zoneItemId = zone_items[i].name;
+      function dropZoneDropActive(selectedField, status) {
+        for (var zone in drop_zones) {
+          var found = false;
+          var zone_items = drop_zones[zone].acceptFields;
+          for (var i = 0; i < zone_items.length; i++) {
+            var zoneItemId = zone_items[i].name;
 
-                 if(zoneItemId === selectedField.name)
-                 {
-                     found = true;
-                     break;
-                 }
-             }
+            if (zoneItemId === selectedField.name) {
+              found = true;
+              break;
+            }
+          }
 
-             if(found)
-             {
-                 drop_zones[zone].active = status;
-             }
-             else {
-                 drop_zones[zone].disabled = status;
-             }
-         }
-     }
+          if (found) {
+            drop_zones[zone].active = status;
+          } else {
+            drop_zones[zone].disabled = status;
+          }
+        }
+      }
 
-     function dropZoneFieldRemove(dropZoneId, fieldId) {
-       for(var zone in drop_zones)
-       {
-         var zoneId = drop_zones[zone].id;
+      function dropZoneFieldRemove(dropZoneId, fieldId) {
+        for (var zone in drop_zones) {
+          var zoneId = drop_zones[zone].id;
 
-         if(zoneId === dropZoneId)
-         {
-           var zone_items = drop_zones[zone].currentFields;
-           for(var i=0; i < zone_items.length; i++)
-           {
-             var zoneItemId = zone_items[i].id;
+          if (zoneId === dropZoneId) {
+            var zone_items = drop_zones[zone].currentFields;
+            for (var i = 0; i < zone_items.length; i++) {
+              var zoneItemId = zone_items[i].id;
 
-             if(zoneItemId === fieldId)
-             {
-               zone_items.splice(i, 1);
-               var field_remove = getFieldById(fieldId);
-               field_remove.state = false;
-               break;
-             }
-           }
-           break;
-         }
-       }
-     }
+              if (zoneItemId === fieldId) {
+                zone_items.splice(i, 1);
+                var field_remove = getFieldById(fieldId);
+                field_remove.state = false;
+                break;
+              }
+            }
+            break;
+          }
+        }
+      }
 
-     function getFieldById(id) {
-         for(var field in fields)
-         {
-             var fieldId = fields[field].id;
+      function getFieldById(id) {
+        for (var field in fields) {
+          var fieldId = fields[field].id;
 
-             if(fieldId === id) {
-                 return fields[field];
-             }
-         }
+          if (fieldId === id) {
+            return fields[field];
+          }
+        }
 
-         return null;
-     }
+        return null;
+      }
 
-     function dropZoneFieldAdd(dropZoneId, fieldId) {
-       for(var zone in drop_zones)
-       {
-           var zoneId = drop_zones[zone].id;
+      function dropZoneFieldAdd(dropZoneId, fieldId) {
+        for (var zone in drop_zones) {
+          var zoneId = drop_zones[zone].id;
 
-           if(zoneId === dropZoneId)
-           {
-               var found = false;
-               var zone_items = drop_zones[zone].currentFields;
-               for(var i=0; i < zone_items.length; i++)
-               {
-                   var zoneItemId = zone_items[i].id;
+          if (zoneId === dropZoneId) {
+            var found = false;
+            var zone_items = drop_zones[zone].currentFields;
+            for (var i = 0; i < zone_items.length; i++) {
+              var zoneItemId = zone_items[i].id;
 
-                   if(zoneItemId === fieldId)
-                   {
-                       found = true;
-                       break;
-                   }
-               }
+              if (zoneItemId === fieldId) {
+                found = true;
+                break;
+              }
+            }
 
-               if( !found ) {
-                   var field_to_add = getFieldById(fieldId);
+            if (!found) {
+              var field_to_add = getFieldById(fieldId);
 
-                   if(field_to_add) {
-                       zone_items.push(field_to_add);
-                       field_to_add.state = true;
-                   }
-               }
-               break;
-           }
-       }
-     }
+              if (field_to_add) {
+                zone_items.push(field_to_add);
+                field_to_add.state = true;
+              }
+            }
+            break;
+          }
+        }
+      }
 
       return {
-       getFields: getFields,
-       getDropZones: getDropZones,
-       getCategories: getCategories,
-       fieldSelected: fieldSelected,
-       dropZoneDropActive:dropZoneDropActive,
-       dropZoneFieldRemove: dropZoneFieldRemove,
-       dropZoneFieldAdd: dropZoneFieldAdd
+        getFields: getFields,
+        getDropZones: getDropZones,
+        getCategories: getCategories,
+        fieldSelected: fieldSelected,
+        dropZoneDropActive: dropZoneDropActive,
+        dropZoneFieldRemove: dropZoneFieldRemove,
+        dropZoneFieldAdd: dropZoneFieldAdd
       }
     }
 
